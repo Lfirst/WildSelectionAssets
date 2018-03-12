c8beb77fcb55e42e1db99db0c0e3214db55312db
Lfirst <lulu101m@yahoo.fr>
2018-03-12 11:04:00 +0100

    DoorCorriger

---
+++ Mechanisme/Door/DoorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	public GameObject door;
+	private bool is_open = false;
+	private float posDoor;
+	private bool upDoor = false;
+
+	void start(){
+		posDoor = door.transform.position.y;
+	}
+
+	void OnTriggerStay(){
+		if (is_open == false && Input.GetKeyDown ("e")) {
+			is_open = true;
+			upDoor = true;
+		} else if (is_open && Input.GetKeyDown ("e")) {
+			is_open = false;
+			upDoor = false;
+		}
+
+	}
+
+	void Update(){
+		if(upDoor == false && door.transform.position.y > posDoor + 2)
+			door.transform.position -= door.transform.up * Time.deltaTime;
+		else if(upDoor && door.transform.position.y < posDoor + 6)
+			door.transform.position += door.transform.up * Time.deltaTime;
+			}
+}
